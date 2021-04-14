c7deb9cee7a6cd373caf331d27d500f04b7e990e
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-14 19:17:16 +0530

    init commit

---
+++ Java Practice/Statements.java
@@ -0,0 +1,16 @@
+
+
+public class Statements{
+    public static void main(String[] args){
+
+        int var = 10;
+        var++;
+        var--;
+        System.out.println("This is a test");
+
+        System.out.println("This is" + 
+                "another" + 
+                "still more.");
+        
+    }
+}
